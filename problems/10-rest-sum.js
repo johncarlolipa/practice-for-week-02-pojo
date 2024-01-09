a194b9fc0e8c4a5835701a8394fbe9f5a44a9edf
John Carlo Lipa <johncarlolipa@gmail.com>
2024-01-09 09:02:26 +0800

    done exercise 10

--- problems/10-rest-sum.js
+++ problems/10-rest-sum.js
@@ -11,6 +11,12 @@ restSum(0); // => 0
 
 function restSum(...otherNums) {
   // Your code here
+  let sum = 0
+  for(let num of otherNums){
+    sum += num;
+  }
+
+  return sum
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
